02df548a2ba357804b31ff96f14155e440bad5f6
Yimika Olotin <holayinkajr@gmail.com>
2021-04-09 17:28:11 +0100

    added seeders for user, product and category models

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function products() {
+        return $this->belongsToMany(Product::class)->withPivot('quantity');
+
+    }
 }
